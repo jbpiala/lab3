ba29e861f24f1d41494b60b807b077beeb15a1f9
John Emmanuel Piala <jbpiala@student.apc.edu.ph>
2024-03-05 20:37:53 +0800

    Edited Files

--- ci4/app/Views/guests/create.php
+++ ci4/app/Views/guests/create.php
@@ -11,20 +11,13 @@
     <br>
 
     <label for="email">Email</label>
-	<input type="input" name="email" value="<?= set_value('email') ?>">
-    <br>
-	
-	<label for="website">Website</label>
-    <textarea name="website" cols="50" rows="1"><?= set_value('website') ?></textarea>
-    <br>
-	
-	<label for="comment">Comment</label>
-    <textarea name="comment" cols="45" rows="4"><?= set_value('comment') ?></textarea>
-    <br>
-	
-	<label for="gender">Gender</label>
-    <textarea name="gender" cols="45" rows="1"><?= set_value('gender') ?></textarea>
+    <input type="input" name="email" value="<?= set_value('email') ?>">
     <br>
 
-    <input type="submit" name="submit" value="New Guest">
+    <label for="comment">Comment</label>
+    <textarea name="comment" cols="45" rows="4"><?= set_value('comment') ?></textarea>
+    <br>
+
+    <input type="submit" name="submit" value="Create guest item">
+
 </form>
--- ci4/app/Views/guests/view.php
+++ ci4/app/Views/guests/view.php
@@ -1,6 +1,3 @@
-<h2><?= esc($guests['title']) ?></h2>
 <p><?= esc($guests['name']) ?></p>
 <p><?= esc($guests['email']) ?></p>
-<p><?= esc($guests['website']) ?></p>
 <p><?= esc($guests['comment']) ?></p>
-<p><?= esc($guests['gender']) ?></p>
